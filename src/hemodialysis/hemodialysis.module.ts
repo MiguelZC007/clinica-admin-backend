4b996cefa20b68f486c61087982c3f7365771922
Miguel Zapata <mazc0023@gmail.com>
2022-09-08 13:49:16 -0400

    error corregido

--- src/hemodialysis/hemodialysis.module.ts
+++ src/hemodialysis/hemodialysis.module.ts
@@ -1,10 +1,12 @@
 import { Module } from '@nestjs/common';
 import { HemodialysisService } from './hemodialysis.service';
 import { HemodialysisController } from './hemodialysis.controller';
+import { HemodialysisSessionModule } from 'src/hemodialysis-session/hemodialysis-session.module';
 
 @Module({
   controllers: [HemodialysisController],
   providers: [HemodialysisService],
   exports: [HemodialysisService],
+  imports: [HemodialysisSessionModule],
 })
 export class HemodialysisModule {}
