c2fb4bb68a65e6875f83a99d83937b2ce02de2e1
Miguel Zapata <mazc0023@gmail.com>
2022-09-08 15:31:51 -0400

    control de error

--- src/hemodialysis/hemodialysis.controller.ts
+++ src/hemodialysis/hemodialysis.controller.ts
@@ -17,6 +17,7 @@ import { ApiBearerAuth, ApiTags } from '@nestjs/swagger';
 import { Auth } from 'src/decorators/auth.decorator';
 import { HemodialysisSessionService } from 'src/hemodialysis-session/hemodialysis-session.service';
 import * as moment from 'moment';
+import { ErrorsManager } from 'src/errors-manager';
 
 @ApiTags('Hemodialisis')
 @ApiBearerAuth()
@@ -43,86 +44,90 @@ export class HemodialysisController {
     @Query('from') from: string,
     @Query('to') to: string,
   ) {
-    const sessions_dto: Prisma.HemodialysisSessionUncheckedCreateInput[] = [];
-    const hemodialysis = await this.hemodialysisService.findUnique({
-      where: { patient_id: patient_id },
-      include: {
-        hemodialysis_machine: {
-          include: {
-            turn_machine: {
-              include: {
-                machine: true,
-                turn: true,
+    try {
+      const sessions_dto: Prisma.HemodialysisSessionUncheckedCreateInput[] = [];
+      const hemodialysis = await this.hemodialysisService.findUnique({
+        where: { patient_id: patient_id },
+        include: {
+          hemodialysis_machine: {
+            include: {
+              turn_machine: {
+                include: {
+                  machine: true,
+                  turn: true,
+                },
               },
             },
           },
         },
-      },
-    });
-    if (hemodialysis?.hemodialysis_machine != null) {
-      const { hemodialysis_machine } = hemodialysis;
-      const { turn_machine } = hemodialysis_machine;
-      const { turn, machine } = turn_machine;
-
-      let d = from;
-      let n = 1;
-      while (moment(d).isBetween(from, to, undefined, '[]')) {
-        if (turn.days.filter((item) => item == moment(d).day()).length > 0) {
-          const r: Prisma.HemodialysisSessionUncheckedCreateInput = {
-            check_in: turn.check_in,
-            check_out: turn.check_out,
-            number_machine: machine.number_machine,
-            number_session: `${n}`,
-            date: d,
-            vascular_access: hemodialysis.vascular_access,
-            hemodialysis_id: hemodialysis.id,
-            dry_weight: 0,
-            income_weight: 0,
-            egress_weight: 0,
-            ultrafiltration_session: '',
-            ultrafiltration_end: '',
-            filter_type: '',
-            filter_reuse: '',
-            line_reuse: '',
-            heparin: '',
-            ktv: '',
-            ingest: '',
-            oxygenation: 0,
-            pa_entry: '',
-            nursing_evaluation: null,
-            clinic_evaluation: null,
-            treatment: null,
-            type_hemodialysis: 'CONVENIO',
-          };
-          sessions_dto.push(r);
-        }
-        d = moment(d).add(1, 'day').toISOString();
-        n++;
-      }
-      if (sessions_dto.length > 0) {
-        await this.hemodialysisSessionService.createMany({
-          data: sessions_dto,
-        } as Prisma.HemodialysisSessionCreateManyArgs);
-      }
-    } else {
-      return new BadRequestException({
-        message: 'El paciente no tiene asignado un turno',
       });
+      if (hemodialysis?.hemodialysis_machine != null) {
+        const { hemodialysis_machine } = hemodialysis;
+        const { turn_machine } = hemodialysis_machine;
+        const { turn, machine } = turn_machine;
+
+        let d = from;
+        let n = 1;
+        while (moment(d).isBetween(from, to, undefined, '[]')) {
+          if (turn.days.filter((item) => item == moment(d).day()).length > 0) {
+            const r: Prisma.HemodialysisSessionUncheckedCreateInput = {
+              check_in: turn.check_in,
+              check_out: turn.check_out,
+              number_machine: machine.number_machine,
+              number_session: `${n}`,
+              date: d,
+              vascular_access: hemodialysis.vascular_access,
+              hemodialysis_id: hemodialysis.id,
+              dry_weight: 0,
+              income_weight: 0,
+              egress_weight: 0,
+              ultrafiltration_session: '',
+              ultrafiltration_end: '',
+              filter_type: '',
+              filter_reuse: '',
+              line_reuse: '',
+              heparin: '',
+              ktv: '',
+              ingest: '',
+              oxygenation: 0,
+              pa_entry: '',
+              nursing_evaluation: null,
+              clinic_evaluation: null,
+              treatment: null,
+              type_hemodialysis: 'CONVENIO',
+            };
+            sessions_dto.push(r);
+            n++;
+          }
+          d = moment(d).add(1, 'day').toISOString();
+        }
+        if (sessions_dto.length > 0) {
+          await this.hemodialysisSessionService.createMany({
+            data: sessions_dto,
+          } as Prisma.HemodialysisSessionCreateManyArgs);
+        }
+      } else {
+        throw new BadRequestException({
+          message: 'El paciente no tiene asignado un turno',
+        });
+      }
+      return this.hemodialysisSessionService.findMany({
+        where: {
+          hemodialysis: {
+            patient_id: patient_id,
+          },
+          date: {
+            gte: from,
+            lte: to,
+          },
+        },
+        orderBy: {
+          number_session: 'asc',
+        },
+      });
+    } catch (e) {
+      ErrorsManager(e);
     }
-    return this.hemodialysisSessionService.findMany({
-      where: {
-        hemodialysis: {
-          patient_id: patient_id,
-        },
-        date: {
-          gte: from,
-          lte: to,
-        },
-      },
-      orderBy: {
-        number_session: 'asc',
-      },
-    });
   }
 
   @Post()
