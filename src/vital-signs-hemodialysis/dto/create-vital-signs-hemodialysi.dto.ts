775b638257c09c9291081544a3fb64693a8b8da6
Miguel Zapata <mazc0023@gmail.com>
2022-09-15 16:20:02 -0400

    dto signos vitales de hemodialisis

--- src/vital-signs-hemodialysis/dto/create-vital-signs-hemodialysi.dto.ts
+++ src/vital-signs-hemodialysis/dto/create-vital-signs-hemodialysi.dto.ts
@@ -1,49 +1,50 @@
-import { ApiProperty } from "@nestjs/swagger"
-import { IsDecimal, IsNotEmpty, IsNumber, IsPositive, IsString, IsUUID } from "class-validator"
+import { ApiProperty } from '@nestjs/swagger';
+import { IsNotEmpty, IsNumber, IsString, IsUUID } from 'class-validator';
 
 export class CreateVitalSignsHemodialysiDto {
-    @ApiProperty()
-    @IsNotEmpty()
-    @IsUUID()
-    hemodialysis_session_id: string
+  @ApiProperty()
+  @IsNotEmpty()
+  @IsUUID()
+  hemodialysis_session_id: string;
 
-    @ApiProperty()
-    @IsNotEmpty()
-    @IsPositive()
-    hour: number
+  @ApiProperty()
+  @IsNotEmpty()
+  @IsNumber()
+  hour: number;
 
-    @ApiProperty()
-    @IsNotEmpty()
-    @IsString()
-    pa: string
+  @ApiProperty()
+  @IsNotEmpty()
+  @IsString()
+  pa: string;
 
-    @ApiProperty()
-    @IsNotEmpty()
-    @IsNumber()
-    fc: number
+  @ApiProperty()
+  @IsNotEmpty()
+  @IsNumber()
+  fc: number;
 
-    @ApiProperty()
-    @IsNotEmpty()
-    @IsDecimal()
-    temp: number
+  @ApiProperty()
+  @IsNotEmpty()
+  @IsNumber()
+  temp: number;
 
-    @ApiProperty()
-    @IsNotEmpty()
-    @IsNumber()
-    blood_flow: number
+  @ApiProperty()
+  @IsNotEmpty()
+  @IsNumber()
+  blood_flow: number;
 
-    @ApiProperty()
-    @IsNotEmpty()
-    @IsNumber()
-    pv: number
+  @ApiProperty()
+  @IsNotEmpty()
+  @IsNumber()
+  pv: number;
 
-    @ApiProperty()
-    @IsNotEmpty()
-    @IsNumber()
-    ptm: number
+  @ApiProperty()
+  @IsNotEmpty()
+  @IsNumber()
+  ptm: number;
 
-    @ApiProperty()
-    @IsNotEmpty()
-    @IsDecimal()
-    conductivity: number
+  @ApiProperty()
+  @IsNotEmpty()
+  @IsNumber()
+  conductivity: number;
 }
+
